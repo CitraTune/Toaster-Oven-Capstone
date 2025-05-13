25f214abd985fc15a8643f628c33cd6813fefd44
James <tacticalyummyness10@gmail.com>
2025-05-13 02:22:22 -0700

    nothing

--- test_fonts/main.cpp
+++ test_fonts/main.cpp
@@ -1,14 +1,14 @@
 #include <LovyanGFX.hpp>
-// #include "UIManager.hpp"
+#include "UIManager.hpp"
 #include "Button.hpp"
 #include "FontSelector.hpp"
-#include <ReflowController.hpp>
+#include "ReflowController.hpp"
 
-// You need to create or access LGFX and UIManager objects first
-LGFX display;           // Create display instance
+
+LGFX display;
 //This display instance is for the LGFX library. 
 //It will be used for all classes that want to interact with the display.
-UIManager uiManager(display);    
+UIManager uiManager(display);
 // Create UIManager instance that handles the touchscreen interface,
 // manages multiple screens (main and settings),
 // also creates the buttons and handles the touch events and actions
